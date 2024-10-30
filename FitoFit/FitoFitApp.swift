8c7a273b6399bb65535a020563353b49770257b1
Frank Liu <fl9971@princeton.edu>
2024-10-29 23:51:21 -0400

    Initial Commit

---
+++ FitoFit/FitoFitApp.swift
@@ -0,0 +1,17 @@
+//
+//  FitoFitApp.swift
+//  FitoFit
+//
+//  Created by Frank Liu on 10/29/24.
+//
+
+import SwiftUI
+
+@main
+struct FitoFitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FitoFitTests/FitoFitTests.swift
@@ -0,0 +1,17 @@
+//
+//  FitoFitTests.swift
+//  FitoFitTests
+//
+//  Created by Frank Liu on 10/29/24.
+//
+
+import Testing
+@testable import FitoFit
+
+struct FitoFitTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
