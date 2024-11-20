eece597ef376474a1b6cc323d674c45fd120116a
Frank Liu <fl9971@princeton.edu>
2024-11-20 01:42:35 -0500

    cropped backgrounds, need to work on filters and image metadata

---
+++ FitoFit/ClothingItem.swift
@@ -0,0 +1,8 @@
+//
+//  ClothingItem.swift
+//  FitoFit
+//
+//  Created by Frank Liu on 11/6/24.
+//
+
+import Foundation
---
+++ FitoFit/ImagePicker.swift
@@ -0,0 +1,8 @@
+//
+//  ImagePicker.swift
+//  FitoFit
+//
+//  Created by Frank Liu on 11/6/24.
+//
+
+import Foundation
